1ccfa17609fd5904705714ddde74b5fcbd51dbc9
Christopher Rosset <chris@rosset.tech>
2023-06-09 23:51:02 -0400

    Use Guile 3.0

--- default.nix
+++ default.nix
@@ -2,11 +2,22 @@
 
 pkgs.mkShell {
   nativeBuildInputs = [
-    pkgs.guile_2_2
+    pkgs.guile_3_0
     pkgs.guile-gcrypt
     pkgs.guile-json
   ];
+
   shellHook = ''
-    echo $GUILE_LOAD_PATH | sed 's/:/\n/g'
+    transform() {
+        for path in $(echo $GUILE_LOAD_PATH | sed 's/:/\n/g'| sort | uniq); do
+            if [ -d "$path/3.0" ]; then
+                echo $path
+            else
+                echo $path/$(ls -1 $path | tail -n 1)
+            fi
+        done
+    }
+
+    export GUILE_LOAD_PATH=$(transform | tr '\n' : | sed 's/:$//')
   '';
 }
